cbf7894f6e8f7eb0a18c320f63bbaf8f3bdccbe6
Nicolas Renck <nicolas.renck@gmail.com>
2026-05-18 18:54:22 -0300

    ajuste03

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS='api-auth-bqyh.onrender.com,localhost,127.0.0.1'
+ALLOWED_HOSTS=api-auth-bqyh.onrender.com,localhost,127.0.0.1
 
 CSRF_TRUSTED_ORIGINS = ['ALLOWED_HOSTS=api-auth-bqyh.onrender.com']
 
